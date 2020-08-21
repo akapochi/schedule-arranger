c0fadcf6ee09d2823ac48acd5391679c25adf09b
Soma Akahoshi <redstar.ihsohaka@gmail.com>
2020-08-21 17:05:41 +0900

    「19. 予定 の作成と表示」の練習問題まで実装

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,8 +1,10 @@
 'use strict';
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
-  'postgres://postgres:postgres@localhost/schedule_arranger'
-);
+  'postgres://postgres:postgres@localhost/schedule_arranger',
+  {
+    logging: false   //ログが出ないようにする
+  });
 
 module.exports = {
   database: sequelize,
--- test/test.js
+++ test/test.js
@@ -1,7 +1,11 @@
+
 'use strict';
-const request = require('supertest');
-const app = require('../app');
-const passportStub = require('passport-stub');
+let request = require('supertest');
+let app = require('../app');
+let passportStub = require('passport-stub');
+let User = require('../models/user');
+let Schedule = require('../models/schedule');
+let Candidate = require('../models/candidate');
 
 describe('/login', () => {
   beforeAll(() => {
@@ -14,27 +18,74 @@ describe('/login', () => {
     passportStub.uninstall(app);
   });
 
-  test('ログインのためのリンクが含まれる', () => {
-    return request(app)
+  it('ログインのためのリンクが含まれる', (done) => {
+    request(app)
       .get('/login')
       .expect('Content-Type', 'text/html; charset=utf-8')
       .expect(/<a href="\/auth\/github"/)
-      .expect(200);
+      .expect(200, done);
   });
 
-  test('ログイン時はユーザー名が表示される', () => {
-    return request(app)
+  it('ログイン時はユーザー名が表示される', (done) => {
+    request(app)
       .get('/login')
       .expect(/testuser/)
-      .expect(200);
+      .expect(200, done);
   });
 });
 
 describe('/logout', () => {
-  test('/ にリダイレクトされる', () => {
-    return request(app)
+  it('/ にリダイレクトされる', (done) => {
+    request(app)
       .get('/logout')
       .expect('Location', '/')
-      .expect(302);
+      .expect(302, done);
   });
+});
+
+describe('/schedules', () => {
+  beforeAll(() => {
+    passportStub.install(app);
+    passportStub.login({ id: 0, username: 'testuser' });
+  });
+
+  afterAll(() => {
+    passportStub.logout();
+    passportStub.uninstall(app);
+  });
+
+  it('予定が作成でき、表示される', (done) => {
+    User.upsert({ userId: 0, username: 'testuser' }).then(() => {
+      request(app)
+        .post('/schedules')
+        .send({ scheduleName: 'テスト予定1', memo: 'テストメモ1\r\nテストメモ2', candidates: 'テスト候補1\r\nテスト候補2\r\nテスト候補3' })
+        .expect('Location', /schedules/)
+        .expect(302)
+        .end((err, res) => {
+          let createdSchedulePath = res.headers.location;
+          request(app)
+            .get(createdSchedulePath)
+            .expect(/テスト予定1/)
+            .expect(/テストメモ1/)
+            .expect(/テストメモ2/)
+            .expect(/テスト候補1/)
+            .expect(/テスト候補2/)
+            .expect(/テスト候補3/)
+            .expect(200)
+            .end((err, res) => {
+              // テストで作成したデータを削除
+              let scheduleId = createdSchedulePath.split('/schedules/')[1];
+              Candidate.findAll({
+                where: { scheduleId: scheduleId }
+              }).then((candidates) => {
+                candidates.forEach((c) => { c.destroy(); });
+                Schedule.findById(scheduleId).then((s) => { s.destroy(); });
+              });
+              if (err) return done(err);
+              done();
+            });
+        });
+    });
+  });
+
 });
